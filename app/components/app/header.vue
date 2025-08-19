626331fb3e34181c17565f4200a187ee3471e8a8
waraphon7939 <waraphon7939@gmail.com>
2025-08-19 12:24:47 +0700

    update

--- app/components/app/header.vue
+++ app/components/app/header.vue
@@ -2,8 +2,12 @@
     <div class="font-[sans-serif] bg-green-200 min-h-screen">
         <!-- Star Header -->
         <div class="text-green-200 bg-[#66C712]">
-            <nav class="container px-6 py-[0.8rem] mx-auto md:justify-between md:items-center shadow-xl pl-5 md:px-30 z-0 curso">
-                <h1>Header</h1>
+            <nav
+                class="container px-6 py-[0.8rem] mx-auto md:flex md:justify-between md:items-center shadow-xl pl-5 md:px-30 z-0 cursor-pointer">
+                <NuxtLink to="/"
+                    class="flex justify-center items-center text-white text-md md:text-2xl font-bold hover:text-blue-200">
+                    <span class="text-center ml-2">Dev</span>
+                </NuxtLink>
             </nav>
         </div>
         <!-- End Header -->
